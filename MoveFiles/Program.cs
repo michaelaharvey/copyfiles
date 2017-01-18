5e45cb05453aa48df6002ac35c306d5f98df51a2
Michael Harvey <michael.harvey@pricespider.com>
2017-01-18 11:28:08 -0800

    update

--- MoveFiles/Program.cs
+++ MoveFiles/Program.cs
@@ -14,7 +14,6 @@ namespace MoveFiles
         static Process processNiad = new Process();
         static string pathlocalplmrelease = ConfigurationManager.AppSettings["pathplmrelease"];
         static string pathniadplm = ConfigurationManager.AppSettings["pathniadplm"];
-        static string pathlocalstoredebug = ConfigurationManager.AppSettings["pathlocalstoredebug"];
         static string pathlocalstoreniad = ConfigurationManager.AppSettings["pathlocalstoreniad"];
         static string pathamrelease = ConfigurationManager.AppSettings["pathamrelease"];
         static string pathniadam = ConfigurationManager.AppSettings["pathniadam"];
@@ -34,13 +33,13 @@ namespace MoveFiles
             do
             {
                 Console.WriteLine("Select a command:");
-                Console.WriteLine("Copy PLM release to niad: 1");
-                Console.WriteLine("Copy AM release to niad: 2");
-                Console.WriteLine("Copy localstore debug to niad: 3");
-                Console.WriteLine("Copy localstore release to processshare: 4");
-                Console.WriteLine("Copy AM release to processshare: 5");
-                Console.WriteLine("Copy crawler release to processshare: 6");
-                Console.WriteLine("exit: e");
+                Console.WriteLine("Copy PLM release to niad: ============================ 1");
+                Console.WriteLine("Copy AM release to niad: ============================= 2");
+                Console.WriteLine("Copy localstore release to niad: ===================== 3");
+                Console.WriteLine("Copy localstore release to processshare: ============= 4");
+                Console.WriteLine("Copy AM release to processshare: ===================== 5");
+                Console.WriteLine("Copy crawler release to processshare: ================ 6");
+                Console.WriteLine("exit: ================================================ e");
 
                 input = Console.ReadLine();
 
@@ -71,20 +70,75 @@ namespace MoveFiles
 
             } while (input != "e");
         }
-
+        
         public static void CopyPLMToNiad()
         {
-            CopyFiles(pathlocalplmrelease, pathniadplm);
+            DisconnectFromVPN();
+            CopyFiles(pathlocalplmrelease, pathniadplm, defaultEndings);
+            ReconnectToVPN();
+        }
+
+        private static void ReconnectToVPN()
+        {
+            Console.WriteLine("Enter VPN Username:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter password to reconnect to VPN: ");
+            string pw = ReadPassword();
+            string command = string.Format(@"""DEV VPN"" ""{0}"" ""{1}""", name, pw);
+            Process.Start("rasdial.exe", command);
+        }
+
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        // remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return password;
+        }
+
+        private static void DisconnectFromVPN()
+        {
+            Process.Start("rasdial.exe", @"""DEV VPN"" /DISCONNECT");
         }
 
         public static void CopyLocalStoreToNiad()
         {
-            CopyFiles(pathlocalstoredebug, pathlocalstoreniad);
+            DisconnectFromVPN();
+            CopyFiles(pathlocalstorerelease, pathlocalstoreniad, defaultEndings);
+            ReconnectToVPN();
         }
 
         public static void CopyAMToNiad()
         {
+            DisconnectFromVPN();
             CopyFiles(pathamrelease, pathniadam, defaultEndings);
+            ReconnectToVPN();
         }
 
         public static void CopyAMToProduction()
@@ -153,10 +207,10 @@ namespace MoveFiles
             CopyFiles(pathcrawlerrelease, pathcrawleramazon, defaultEndings);
         }
 
-        public static void CopyFiles(string sourcePath, string destinationPath)
-        {
-            CopyFiles(sourcePath, destinationPath, null);
-        }
+        //public static void CopyFiles(string sourcePath, string destinationPath)
+        //{
+        //    CopyFiles(sourcePath, destinationPath, null);
+        //}
 
         public static void CopyFiles(string sourcePath, string destinationPath, string[] endings)
         {
@@ -185,6 +239,15 @@ namespace MoveFiles
                     }
                 }
 
+                for (int i = 0; i < filteredFiles.Count; i++)
+                {
+                    if (filteredFiles[i].Contains("vshost.exe"))
+                    {
+                        filteredFiles.Remove(filteredFiles[i]);
+                        break;
+                    }
+                }
+
                 foreach (string s in filteredFiles)
                 {
                     string filename = System.IO.Path.GetFileName(s);
@@ -199,7 +262,7 @@ namespace MoveFiles
                         Console.WriteLine(e.Message);
                         Console.WriteLine(e.InnerException);
                         Console.WriteLine(e.StackTrace);
-                        break;
+                        //break;
                     }
                 }
             }
@@ -207,8 +270,6 @@ namespace MoveFiles
             {
                 Console.WriteLine("Source path does not exist!");
             }
-
-
         }
     }
 }
